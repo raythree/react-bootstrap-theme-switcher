bd858da90a7c674d5486978d9a4d1e1b7013e7de
raythree <github@raythree.com>
2016-08-03 18:44:05 -0400

    remove themeService

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-var themeService = require('./lib/themeService').themeService;
 var ThemeChooser = require('./lib/ThemeChooser').ThemeChooser;
 var ThemeSwitcher = require('./lib/ThemeSwitcher').ThemeSwitcher;
 
 module.exports = {
-  themeService: themeService,
   ThemeChooser: ThemeChooser,
   ThemeSwitcher: ThemeSwitcher
 };
